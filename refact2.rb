d9e379a05bb666d9b7b3765668e05b265bdc5dd1
codeamac03 <codeamac03@gmail.com>
2016-08-09 20:42:58 -0500

    ejercicio de refactorizar 1

---
+++ refact2.rb
@@ -0,0 +1,10 @@
+#Refactoring Unless
+#Corrige y refactoriza 
+def swim(time)
+  #el metodo swim regresa la leyenda utilizando unless como if not
+  p "#{time} seconds... New Record for 50 meters" unless time > 10
+end
+
+#test
+p swim(5) == "5 seconds... New Record for 50 meters"
+p swim(8) == "8 seconds... New Record for 50 meters"
